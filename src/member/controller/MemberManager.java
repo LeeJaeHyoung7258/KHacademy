dc678c7b71d9970c94d5fe3919157d8b68752567
user <user@DESKTOP-C34L5NL>
2020-03-23 21:47:01 +0900

    ing MemberManager
    MemberManager 하는 중

--- src/member/controller/MemberManager.java
+++ src/member/controller/MemberManager.java
@@ -6,7 +6,7 @@ import member.model.vo.Member;
 
 public class MemberManager {
 	
-	private static Member[] marr = new Member[10];
+	private static Member[] marr = new Member[2];
 	private static int ctn = 0;
 	private Scanner sc = new Scanner(System.in);
 	
@@ -14,10 +14,36 @@ public class MemberManager {
 	
 	public void insertMember() {
 		
+		for(int i = 0; i < marr.length; i++) {
+			System.out.print("아이디 입력 : ");
+			String id = sc.nextLine();
+			System.out.print("비밀번호 입력 : ");
+			String pwd = sc.nextLine();
+			System.out.print("이름 : ");
+			String name = sc.nextLine();
+			System.out.print("나이 : ");
+			int age = sc.nextInt();
+			System.out.print("성별 : ");
+			char gender = sc.next().charAt(0);
+			sc.nextLine();
+			System.out.print("이메일 : ");
+			String email = sc.nextLine();
+			System.out.println();
+			
+			marr[i] = new Member(id, pwd, name, age, gender, email);
+			++ctn;
+		}
+		System.out.println("입력이 완료되었습니다. 메인 메뉴로 돌아갑니다.");
+		System.out.println(ctn);
+		
 	}
 	
 	public void searchId() {
-		
+		System.out.print("검색할 아이디를 입력하세요 : ");
+		String srch = sc.nextLine();
+		for(int i = 0; i < marr.length; i++) {
+//			marr[i].getUserId()
+		}
 	}
 	
 	public void searchName() {
@@ -53,6 +79,7 @@ public class MemberManager {
 	}
 	
 	public void printOne(Member m) {
+		System.out.println();
 		
 	}
 
