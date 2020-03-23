f1e1be55989c5c9fa9d1f3b15835434daaaf0786
user <user@DESKTOP-C34L5NL>
2020-03-23 20:52:07 +0900

    made class MemberMenu
    MemberMenu 클래스 생성

--- src/member/view/MemberMenu.java
+++ src/member/view/MemberMenu.java
@@ -1,5 +1,16 @@
 package member.view;
 
+import java.util.Scanner;
+
+import member.controller.MemberManager;
+
 public class MemberMenu {
+	
+	private Scanner sc = new Scanner(System.in);
+	private MemberManager mm = new MemberManager();
+	
+	public MemberMenu() {}
+	
+	
 
 }
